2a65f81808280c189c7d12ed2ac8c95f452c5fa9
alexf <alexf@tut.by>
2024-09-30 13:02:26 +0300

    7-2 - done

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,8 +4,23 @@
  */
 
 function isNumeric(str) {
-  // your code
+  numbers = '1234567890'
+  answer = true
+  for (n of str) {
+    if (numbers.includes(n)) {
+      answer = true
+    } else {
+      return false
+    }
+
+  }
+  return answer
+ 
 }
+
+console.log(isNumeric("1234"));
+
+
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
